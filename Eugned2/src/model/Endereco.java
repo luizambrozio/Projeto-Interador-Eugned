f555e723f13bdaffbdddd85e9428904fd15c1ec9
ambrozio <oliveira.ambrozio@gmail.com>
2016-06-07 21:19:01 -0300

    criado entidades Endereco e Paciente

--- Eugned2/src/model/Endereco.java
+++ Eugned2/src/model/Endereco.java
@@ -2,6 +2,69 @@ package model;
 
 public class Endereco {
 	
+	private Integer id;
+	private String rua;
+	private String numero;
+	private String bairro;
+	private String cep;
+	private String cidade;
+	private String estado;
+	
+	public Endereco(Integer id, String rua, String numero, String bairro, String cep, String cidade, String estado) {
+		super();
+		this.id = id;
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
 	
 
 }
--- Eugned2/src/model/Paciente.java
+++ Eugned2/src/model/Paciente.java
@@ -1,5 +1,99 @@
 package model;
 
+import java.util.Date;
+
 public class Paciente {
+	
+	private int id;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String escolaridade;
+	private String estadoCivil;
+	private String sexo;
+	private String corRaca;
+	private Date dataNascimento;
+	private Integer rendaFamiliar;
+	
+	
+	public Paciente(int id, String nome, String cpf, String rg, String escolaridade, String estadoCivil, String sexo,
+			String corRaca, Date dataNascimento, Integer rendaFamiliar) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.escolaridade = escolaridade;
+		this.estadoCivil = estadoCivil;
+		this.sexo = sexo;
+		this.corRaca = corRaca;
+		this.dataNascimento = dataNascimento;
+		this.rendaFamiliar = rendaFamiliar;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getEscolaridade() {
+		return escolaridade;
+	}
+	public void setEscolaridade(String escolaridade) {
+		this.escolaridade = escolaridade;
+	}
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getCorRaca() {
+		return corRaca;
+	}
+	public void setCorRaca(String corRaca) {
+		this.corRaca = corRaca;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Integer getRendaFamiliar() {
+		return rendaFamiliar;
+	}
+	public void setRendaFamiliar(Integer rendaFamiliar) {
+		this.rendaFamiliar = rendaFamiliar;
+	}
+	
+	
+	
+	
 
 }
