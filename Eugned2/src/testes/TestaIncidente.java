98c3b0657ede47f4a17df5c7c109919ef83521f1
wagmattei <wagmattei@wagmattei-RV411-RV511-E3511-S3511-RV711>
2016-06-16 19:18:10 -0300

    Ajuste em Incidente e Paciente Dao

--- Eugned2/src/testes/TestaIncidente.java
+++ Eugned2/src/testes/TestaIncidente.java
@@ -13,24 +13,28 @@ public class TestaIncidente {
 
 	public static void main(String[] args) {
 		// Testando Entidade incidente e Endereco
-		//testaListar();
-		 testaInserir();
+		testaInserir();
+		testaListar();
 
 }
-
 	private static void testaListar() {
 	for (Incidente incidente : new IncidenteDAO().getListaIncidentes()) {
-		System.out.println(incidente.getPaciente().getNome());
+		System.out.println(incidente.getDataIncidente().toLocaleString() +
+						" - " + incidente.getDataSintoma().toLocaleString() +
+						" - " + incidente.getSintomas());
 	}
 		
 }
 
 	private static void testaInserir() {
+		// Conecta o PacienteDAO
 		PacienteDAO pacienteDAO = new PacienteDAO();
-		int id = 1;
-		Paciente paciente = (pacienteDAO.getPacienteById(id));
+		int idPaciente = 3;
+		// Buscao o Paciente com id 1 e cria o objeto paciente
+		Paciente paciente = (pacienteDAO.getPacienteById(idPaciente));
 		System.out.println("Paciente: "+paciente.getNome());
 		
+		// Criar um objeto Incidente
 		Incidente incidente = new Incidente();
 		incidente.setDataIncidente(new java.util.Date());
 		incidente.setDataSintoma(new java.util.Date());
@@ -38,21 +42,38 @@ public class TestaIncidente {
 		incidente.setSintomas("sintomas");
 		System.out.println("incidente: " + incidente.getDataIncidente() + " - " + paciente.getNome());
 		
+		// Conectar o IncidenteDAO e inserir o objeto Incidente
 		new IncidenteDAO().inserir(incidente);
+		System.out.println("Id incidente: " + incidente.getId());
 		System.out.println(new IncidenteDAO().getListaIncidentes());
 		
-
+		// Conecta o EnderecoDao
 		EnderecoDao enderecoDao = new EnderecoDao();
-		Endereco endereco = (enderecoDao.getEnderecoById(id));
-		System.out.println("Endereco: "+endereco.getRua());
+		//Busca o Endereco pelo id
+		Endereco endereco1 = (enderecoDao.getEnderecoById(4));
+		Endereco endereco2 = (enderecoDao.getEnderecoById(7));
+		System.out.println("Endereco1: "+endereco1.getRua());
+		System.out.println("Endereco2: "+endereco2.getRua());
 		
-		IncidenteEndereco incidenteEndereco = new IncidenteEndereco();
-		incidenteEndereco.setIncidente(incidente);
-		incidenteEndereco.setEndereco(endereco);
-		System.out.println("incidente: " + incidenteEndereco.getIncidente() + " - " + endereco.getRua());
+		// Criar um Objeto incidenteEndereco
+		IncidenteEndereco incidenteEndereco1 = new IncidenteEndereco();
+		incidenteEndereco1.setIncidente(incidente);
+		incidenteEndereco1.setEndereco(endereco1);
+
+		// Criar um Objeto incidenteEndereco
+		IncidenteEndereco incidenteEndereco2 = new IncidenteEndereco();
+		incidenteEndereco2.setIncidente(incidente);
+		incidenteEndereco2.setEndereco(endereco2);
+		System.out.println("incidente1: " + incidenteEndereco1.getIncidente() + " - " + incidenteEndereco1.getEndereco().getRua());
+		System.out.println("incidente2: " + incidenteEndereco2.getIncidente() + " - " + incidenteEndereco2.getEndereco().getRua());
 		
-		new IncidenteEnderecoDAO().inserir(incidenteEndereco);
-		System.out.println(new IncidenteEnderecoDAO().getListaincidenteEnderecos());
+		// Conecta o Incideten Dao e insere o objeto incidenteEndereco
+		new IncidenteEnderecoDAO().inserir(incidenteEndereco1);
+		new IncidenteEnderecoDAO().inserir(incidenteEndereco2);
+		
+		
+		
+		System.out.println("Gravou");
 		
 	}
 
