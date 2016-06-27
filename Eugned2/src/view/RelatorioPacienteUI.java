209cfbb33ceed479ace83d67db1391ed563cf523
wagmattei <wagmattei@wagmattei-RV411-RV511-E3511-S3511-RV711>
2016-06-27 19:52:41 -0300

    Ajustes no Filtro do Relatório de Pacientes

--- Eugned2/src/view/RelatorioPacienteUI.java
+++ Eugned2/src/view/RelatorioPacienteUI.java
@@ -2,7 +2,10 @@ package view;
 
 import java.awt.EventQueue;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JInternalFrame;
 import javax.swing.GroupLayout;
@@ -31,6 +34,9 @@ public class RelatorioPacienteUI extends JInternalFrame {
 	private JFormattedTextField jtdataInicialP;
 	private JFormattedTextField jtdatafimP;
 	private MaskFields maskFields = new MaskFields();
+	private SimpleDateFormat formatData = new SimpleDateFormat("dd/MM/yyyy");
+	private JRadioButton rdbtnTodos;
+	private JRadioButton rdbtnDiagnosticadosEmUm;
 
 	/**
 	 * Launch the application.
@@ -53,7 +59,7 @@ public class RelatorioPacienteUI extends JInternalFrame {
 	 */
 	public RelatorioPacienteUI() {
 		setClosable(true);
-		setBounds(100, 100, 486, 197);
+		setBounds(100, 100, 486, 218);
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Relat\u00F3rio Pacientes", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
@@ -62,14 +68,14 @@ public class RelatorioPacienteUI extends JInternalFrame {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 457, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(73, Short.MAX_VALUE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(34, Short.MAX_VALUE))
 		);
 		
 		JLabel lblInformeOPeriodo = new JLabel("Informe o periodo");
@@ -99,47 +105,70 @@ public class RelatorioPacienteUI extends JInternalFrame {
 		JButton jbImprimir = new JButton("Imprimir");
 		jbImprimir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				HashMap hm = new HashMap<>();
+				Map hm = new HashMap<>();
 				
 				try {
-					JasperPrint	jp = JasperFillManager.fillReport("/home/wagmattei/git/Projeto-Interador-Eugned/Eugned2/src/view/RelatorioPaciente2.jasper", new HashMap<>(),ConnectionUtil.getConnection());
+					
+					Date dataInicial = new Date();
+					Date dataFinal = new Date();
+					if(rdbtnTodos.isSelected()) {
+						hm.put("paramFiltrar", false);
+					} else {
+						hm.put("paramFiltrar", true);				    	
+						dataFinal = formatData.parse(jtdatafimP.getText());
+						dataInicial = formatData.parse(jtdataInicialP.getText());
+					}
+					// /home/wagmattei/git/Projeto-Interador-Eugned/Eugned2/src/
+				    hm.put("paramDataInicial", dataInicial);
+				    hm.put("paramDataFinal", dataFinal);
+					JasperPrint	jp = JasperFillManager.fillReport("/home/wagmattei/git/Projeto-Interador-Eugned/Eugned2/src/view/RelatorioPaciente2.jasper", hm,ConnectionUtil.getConnection());
 					JasperViewer.viewReport(jp, false);
+				} catch (ParseException e2) {
+					JOptionPane.showMessageDialog(null, "Data inválida");
 				} catch (JRException e1) {
-					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog(null, "Problemas ao Carregar o Relatório.");
 					e1.printStackTrace();
 				}
-				
+					
 			}
 		});
 		
 		JButton jbVoltar = new JButton("Voltar");
+		
+		JPanel Filtro = new JPanel();
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblInformeOPeriodo)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(jtdataInicialP, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(jbCalendarioFim)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(jtdatafimP, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(jbcalendarFim)
-					.addContainerGap(77, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-					.addContainerGap(238, Short.MAX_VALUE)
-					.addComponent(jbImprimir)
-					.addGap(18)
-					.addComponent(jbVoltar)
-					.addGap(84))
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(lblInformeOPeriodo)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(jtdataInicialP, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(jbCalendarioFim)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(jtdatafimP, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(jbcalendarFim))
+								.addGroup(gl_panel.createSequentialGroup()
+									.addComponent(jbImprimir)
+									.addGap(18)
+									.addComponent(jbVoltar)
+									.addGap(68)))
+							.addGap(16))
+						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+							.addComponent(Filtro, GroupLayout.PREFERRED_SIZE, 435, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(28)
+					.addComponent(Filtro, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(17)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblInformeOPeriodo)
 						.addComponent(jtdataInicialP, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
@@ -150,8 +179,15 @@ public class RelatorioPacienteUI extends JInternalFrame {
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(jbImprimir)
 						.addComponent(jbVoltar))
-					.addContainerGap(59, Short.MAX_VALUE))
+					.addContainerGap(14, Short.MAX_VALUE))
 		);
+		
+		rdbtnTodos = new JRadioButton("Todos");
+		rdbtnTodos.setSelected(true);
+		Filtro.add(rdbtnTodos);
+		
+		rdbtnDiagnosticadosEmUm = new JRadioButton("Diagnosticados em um Período");
+		Filtro.add(rdbtnDiagnosticadosEmUm);
 		panel.setLayout(gl_panel);
 		getContentPane().setLayout(groupLayout);
 
